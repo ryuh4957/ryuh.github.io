81c9def7206fed711700a3cccbab321b0cdff101
ryuh4957 <147961638+ryuh4957@users.noreply.github.com>
2023-10-15 15:00:39 +0900

    background.js

--- background.js
+++ background.js
@@ -10,8 +10,7 @@ function paintImage(imgNumber){
     const image = new Image();
     image.src = `images/${imgNumber}.jpg`;
     image.classList.add('bgImage');
-    body.prepend(image); // body 맨 앞에 추가
-    // image.addEventListener("loadend", handleImgLoad);
+    body.prepend(image);
 
 }
 
